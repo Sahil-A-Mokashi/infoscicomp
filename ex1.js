789a32d05568a6eb1a7c638330b8ced395dc8906
Sahil Mokashi <SahilMokashi6.1@gmail.com>
2026-05-07 04:17:49 +0530

    ex 1 remove weight

--- ex1.js
+++ ex1.js
@@ -22,3 +22,27 @@ let check = () => {
     }
   };
   
+
+  // Function to remove weight
+let remove = () => {
+    let weight = parseInt(document.getElementById("weight").value);
+    let removedWeight = parseInt(document.getElementById("removed").value);
+  
+    // Validate input
+    if (isNaN(removedWeight)) {
+      alert("Please enter weight to remove");
+      return;
+    }
+  
+    let finalWeight = weight - removedWeight;
+  
+    alert("Removing weight: " + removedWeight + "kg");
+    alert("Updated baggage weight: " + finalWeight + "kg");
+  
+    // Check final weight
+    if (finalWeight <= 15) {
+      alert("Baggage weight is now OK to proceed!");
+    } else {
+      alert("Still overweight! Remove more items.");
+    }
+  };
