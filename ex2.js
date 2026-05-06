c08b4a9e6590165fd45b10ce935ead2703049078
Sahil Mokashi <SahilMokashi6.1@gmail.com>
2026-05-07 04:19:49 +0530

    ex2 question

---
+++ ex2.js
@@ -0,0 +1,7 @@
+//redefine these - ex1 should output the solution to project Euler Q1
+//https://projecteuler.net/problem=1
+//ex2 should output the sum of all multiples of a or b less than n
+
+
+// Project Euler Question 1
+// Sum of all multiples of 3 or 5 below 1000
