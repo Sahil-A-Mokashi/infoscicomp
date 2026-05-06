9594e4bb1a3ec4fb174ac3d5c531f9f30710c550
Sahil Mokashi <SahilMokashi6.1@gmail.com>
2026-05-07 04:20:37 +0530

    exercise 2 - euler 1

--- ex2.js
+++ ex2.js
@@ -5,3 +5,15 @@
 
 // Project Euler Question 1
 // Sum of all multiples of 3 or 5 below 1000
+
+let euler1 = () => {
+    let sum = 0;
+
+    for (let i = 1; i < 1000; i++) {
+        if (i % 3 === 0 || i % 5 === 0) {
+            sum += i;
+        }
+    }
+
+    alert("Sum of multiples of 3 or 5 below 1000 is: " + sum);
+};
