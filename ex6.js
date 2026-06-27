9c8c42d6e7879a94e755e0e74574450dac1a1ec4
Sahil-A-Mokashi <101308350+Sahil-A-Mokashi@users.noreply.github.com>
2026-06-28 04:01:32 +0530

    Implement ex6.js mostForkedRepo function
    Add function to fetch and log the most forked repo for a GitHub user.

--- ex6.js
+++ ex6.js
@@ -2,3 +2,20 @@
 //and outputs their repo with the most forks.
 
 //https://api.github.com/
+async function mostForkedRepo(username) {
+  const repos = await fetch(`https://api.github.com/users/${username}/repos`)
+    .then(res => res.json());
+
+  let max = repos[0];
+
+  for (let repo of repos) {
+    if (repo.forks_count > max.forks_count) {
+      max = repo;
+    }
+  }
+
+  console.log(max.name);
+}
+
+mostForkedRepo("sahil-a-mokashi");
+// ecom
